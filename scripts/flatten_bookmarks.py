db794b5fe6b9258e0a7f344ec0a077a6bd5d3381
LPX1138 <85242100+LPX1138@users.noreply.github.com>
2025-02-27 20:48:14 -0500

    Update flatten_bookmarks.py

--- scripts/flatten_bookmarks.py
+++ scripts/flatten_bookmarks.py
@@ -7,7 +7,7 @@ def extract_links(content, levels, current_level=0):
 
     bookmarks = []
     link_pattern = re.compile(r'<DT><A HREF="[^"]+" ADD_DATE="\d+">[^<]+<\/A>')
-    nested_pattern = re.compile(r'DL><p>\s*<DT><H3 ADD_DATE="\d+" LAST_MODIFIED="\d+">[^<]+<\/H3>\s*<DL><p>\s*<DT><A HREF="[^"]+" ADD_DATE="\d+">[^<]+<\/A>\s*<\/DL><p>', re.DOTALL)
+    nested_pattern = re.compile(r'<DL><p>\s*<DT><H3 ADD_DATE="\d+" LAST_MODIFIED="\d+">[^<]+<\/H3>\s*<DL><p>(.*?)<\/DL><p>', re.DOTALL)
 
     for match in link_pattern.finditer(content):
         bookmarks.append(match.group())
